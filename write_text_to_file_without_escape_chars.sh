35280c9fdc4a174810ec1dc7638badb38cc9fda4
richelbilderbeek <richel@richelbilderbeek.nl>
2023-07-17 08:38:08 +0200

    Selfdoc

--- write_text_to_file_without_escape_chars.sh
+++ write_text_to_file_without_escape_chars.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 cat > "test_file.txt" << EOF
-Here I can write
-whatever I want
-Like " and ' and , and /
-and I don't need escape characters!
-It is great!
+Here I can write whatever I want:
+Like " and ' and , and / and I don't need escape characters!
 EOF
 
 cat >> "test_file.txt" << EOF
-... and again
-and again
-whatever I want
-Like " and ' and , and /
-and I don't need escape characters!
-It is great!
+... and again, using EOF as an ID for the second time
 EOF
+
+cat >> "test_file.txt" << EOF314
+... I can also use EOF314 as an ID :-)
+EOF314
 
 cat test_file.txt
 rm test_file.txt
