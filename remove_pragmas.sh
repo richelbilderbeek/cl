fdb4f5e9028da41211c09b57e699ddde4db94c23
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-16 16:43:09 +0200

    Fix style

--- remove_pragmas.sh
+++ remove_pragmas.sh
@@ -2,37 +2,36 @@
 
 for folder in `ls -d */`
 do
+  (
+    cd $folder
 
-  cd $folder
+    perl -pi -w -e 's/#pragma GCC diagnostic pop//g;' ./*..h
+    perl -pi -w -e 's/#pragma GCC diagnostic pop//g;' ./*..cpp
 
-  perl -pi -w -e 's/#pragma GCC diagnostic pop//g;' *.h
-  perl -pi -w -e 's/#pragma GCC diagnostic pop//g;' *.cpp
+    perl -pi -w -e 's/#pragma GCC diagnostic push//g;' ./*..h
+    perl -pi -w -e 's/#pragma GCC diagnostic push//g;' ./*..cpp
 
-  perl -pi -w -e 's/#pragma GCC diagnostic push//g;' *.h
-  perl -pi -w -e 's/#pragma GCC diagnostic push//g;' *.cpp
-
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-variable"//g;' *.h
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-variable"//g;' *.cpp
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-variable"//g;' ./*..h
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-variable"//g;' ./*..cpp
 
 
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-variable"//g;' *.h
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-variable"//g;' *.cpp
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-variable"//g;' ./*..h
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-variable"//g;' ./*..cpp
 
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Weffc\+\+"//g;' *.h
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Weffc\+\+"//g;' *.cpp
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Weffc\+\+"//g;' ./*..h
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Weffc\+\+"//g;' ./*..cpp
 
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-local-typedefs"//g;' *.h
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-local-typedefs"//g;' *.cpp
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-local-typedefs"//g;' ./*..h
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-local-typedefs"//g;' ./*..cpp
 
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"//g;' *.h
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"//g;' *.cpp
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"//g;' ./*..h
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"//g;' ./*..cpp
 
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-but-set-variable"//g;' *.h
-  perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-but-set-variable"//g;' *.cpp
-  git add --all :/
-  git commit -m "Remove pragma's"
-  git push
- 
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-but-set-variable"//g;' ./*..h
+    perl -pi -w -e 's/#pragma GCC diagnostic ignored "-Wunused-but-set-variable"//g;' ./*..cpp
+    git add --all :/
+    git commit -m "Remove pragma's"
+    git push
+  )
 
-  cd ..
 done
