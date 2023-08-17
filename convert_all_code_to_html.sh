22da2ecc4d85c5df23d76c9c46d4228aa7126a45
richelbilderbeek <richel@richelbilderbeek.nl>
2023-08-17 17:21:43 +0200

    Fix style

--- convert_all_code_to_html.sh
+++ convert_all_code_to_html.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 CODE_TO_HTML_BIN=~/bin/ToolCodeToHtmlConsole
 
-if [ ! -e $CODE_TO_HTML_BIN ]
+if [ ! -e ${CODE_TO_HTML_BIN} ]
 then
   echo "ERROR: CodeToHtml binary not found at "${CODE_TO_HTML_BIN}
   exit
 fi
 
-for folder in `ls | egrep -v "build-"`
+for folder in $(ls | egrep -v "build-")
 do
-  echo $folder
-  $CODE_TO_HTML_BIN ./$folder
+  echo "${folder}"
+  ${CODE_TO_HTML_BIN} "${folder}"
 done
 
 rm tmp.txt
