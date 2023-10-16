bb9c0ec1950219a203931ee5359d0affc76dfd2e
richelbilderbeek <richel@richelbilderbeek.nl>
2023-10-16 10:22:08 +0200

    Can get simple prompt

---
+++ get_simple_prompt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Usage:
+#
+#   source get_simple_prompt.sh
+#
+export PS1='\n\w\$ '
+
+echo "This is how to use this script:"
+echo " "
+echo "  source get_simple_prompt.sh"
+echo " "
