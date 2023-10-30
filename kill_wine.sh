3143d1f30bdb817c710d3f246209df52a060c2f0
richelbilderbeek <richel@richelbilderbeek.nl>
2023-10-30 08:54:07 +0100

    Fix style

--- kill_wine.sh
+++ kill_wine.sh
@@ -4,4 +4,4 @@
 # From https://askubuntu.com/a/928094
 # From https://askubuntu.com/a/732320
 filenames=$(ls -l /proc/*/exe 2>/dev/null)
-echo $filename | grep -E 'wine(64)?-preloader|wineserver' | perl -pe 's;^.*/proc/(\d+)/exe.*$;$1;g;' | xargs -n 1 kil
+echo "${filenames}" | grep -E 'wine(64)?-preloader|wineserver' | perl -pe 's;^.*/proc/(\d+)/exe.*$;$1;g;' | xargs -n 1 kil
